d7f3f242eabaed0a71201386b2c6f7a2d685349b
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-02 15:03:41 +0530

    Add files via upload

---
+++ uservalidation.java
@@ -0,0 +1,13 @@
+class example1{
+     public static void main(String[] args){
+        String userid="user001";
+        String password="user@001";
+        if (userid=="user001" & password=="user@001" ){
+            System.out.println("Happer New Year");
+        }
+        else {
+            System.out.println("Invalid user or password");
+        }
+ 
+    }
+}
