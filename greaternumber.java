e71cc9a99dd5473a692781257b74a6dbce50f352
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-02 14:16:36 +0530

    Add files via upload

---
+++ greaternumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class example{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);l
+        int num1=a;
+        int num2=b;
+        if(num1<num2){
+            System.out.println(num2);
+        }
+        else{
+            System.out.println(num1);
+        }
+    }
+}
