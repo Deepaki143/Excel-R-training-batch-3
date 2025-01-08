04e56477b9ac658613329a46c581c5904330e14d
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-08 11:42:40 +0530

    Add files via upload

---
+++ Demo020.java
@@ -0,0 +1,10 @@
+public class Demo020 {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + " + " + num + " = " + (num + num));
+            num = num + num;
+        }
+    }
+}
+
