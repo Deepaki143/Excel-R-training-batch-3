e7f53bea3621afb4946c4ea82a9417f7c7448a8c
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-11 09:34:31 +0530

    Add files via upload

---
+++ Demo024.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class FriendList {
+    ArrayList<String[]> friends = new ArrayList<>();
+
+    public FriendList() {
+        friends.add(new String[] {"Mahesh", "7842517366", "Hyderabad"});
+        friends.add(new String[] {"Deepak", "56464565410", "Hyderabad"});
+        friends.add(new String[] {"Reavanth", "6454546565", "Hyderabad"});
+        friends.add(new String[] {"Bhanu", "9012345678", "Hyderabad"});
+        friends.add(new String[] {"Bharath", "1112223333", "India"});
+    }
+
+    public void displayFriends() {
+        for (String[] friend : friends) {
+            System.out.println("Name: " + friend[0]);
+            System.out.println("Phone Number: " + friend[1]);
+            System.out.println("Address: " + friend[2]);
+            System.out.println();
+        }
+    }
+}
+
+public class Demo024 {
+    public static void main(String[] args) {
+        FriendList friendList = new FriendList();
+        friendList.displayFriends();
+    }
+}
