bb8d8c32c1e4feceaadf0333ea7a0902949c48a9
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-11 10:40:11 +0530

    Minutes to Years convert

--- Demo018.java
+++ Demo018.java
@@ -7,7 +7,7 @@ public class Demo018 {
         int minutes = scanner.nextInt();
         scanner.close();
 
-        int years = minutes / 525600; // 525600 minutes in a non-leap year
+        int years = minutes / 525600; 
         int remainingMinutes = minutes % 525600;
 
         System.out.println(minutes + " minutes is approximately " + years + " years");
