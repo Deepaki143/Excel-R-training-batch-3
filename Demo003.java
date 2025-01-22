a016911388e5a7e9dad0b3fe1b26f9dd0a75bc8e
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-22 18:27:42 +0530

    Greatest among 3 numbers

--- Demo003.java
+++ Demo003.java
@@ -1,17 +1,21 @@
-class example{
-     public static void main(String[] args){
-        int num1=10;
-        int num2=20;
-        int num3=30;
-        if (num1 < num2 & num3 < num2 ){
-            System.out.println(num2);
-        }
-        else if(num1 < num2 & num2 < num3){
-            System.out.println(num3);
-        }
-        else {
-            System.out.println(num1);
-        }
- 
+package Core_Java;
+
+import java.util.Scanner;
+
+class Demo003{
+    public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+            System.out.println("Enter 3 numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if(a>b&&a>c){
+                System.out.println(a+" is greater than "+b+" and "+c);
+            }else if(b>a&&b>c){
+                System.out.println(b+" is greater than "+a+" and "+c);
+            }else{
+                System.out.println(c+" is greater than "+a+" and "+b);
+            }
     }
 }
