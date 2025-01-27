407c5b4df6292198e267a7dc153cff20bada33ed
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-01-27 12:38:50 +0530

    Remove duplicates from array

--- LTcode006.java
+++ LTcode006.java
@@ -1,14 +1,13 @@
 public class LTcode006 {
     public static void main(String[] args) {
-        LTcode006 solution = new LTcode006();
+        LTcode006 LTcode006 = new LTcode006();
         int[] nums = {1, 1, 2, 2, 3, 3, 4, 5};
-        int k = solution.removeDuplicates(nums);
+        int k = LTcode006.removeDuplicates(nums);
         System.out.println("Non-duplicate values: ");
         for (int i = 0; i < k; i++) {
             System.out.print(nums[i] + " ");
         }
     }
-
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
             return 0;
