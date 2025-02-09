8c69069e7b770bbc8188dd70b0f4d1affa1e1c22
Deepak kumar <164378131+Deepaki143@users.noreply.github.com>
2025-02-09 19:30:51 +0530

    Update Demo013.java

--- Demo013.java
+++ Demo013.java
@@ -1,19 +1,21 @@
- package Core_Java;
- class CircleAreaDemo1 {
-
-	void Area() {
-		
-		int r = 5;
-		
-		double ca = Math.PI* Math.pow(r, 2);
-		
-		System.out.println("The circle area is : " + ca);
-	}
-
-}
-public class Demo012 {
+public class Demo013 {
+    private double base;
+    private double height;
+    public Demo013(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+    @Override
+    public String toString() {
+        return "Triangle [base=" + base + ", height=" + height + "]";
+    }
     public static void main(String[] args) {
-        CircleAreaDemo1 obj = new CircleAreaDemo1();
-        obj.Area();
-    }
+        Demo013 triangle = new Demo013(10, 5);
+        double area = triangle.calculateArea();
+        System.out.println(triangle);
+        System.out.println("The area of the triangle is: " + area);
+    }
 }
